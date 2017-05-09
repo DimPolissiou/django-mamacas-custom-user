f9bd48d0d0029ff7fda90093bb7e4640762893f3
DimPolissiou <dimpolissiou@gmail.com>
2017-05-09 18:26:30 +0300

    Update example_settings.py

--- mamacas/example_settings.py
+++ mamacas/example_settings.py
@@ -78,7 +78,7 @@ WSGI_APPLICATION = 'mamacas.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': 'mydatabase',
+        'NAME': 'mamacasdatabase',
     }
 }
 
